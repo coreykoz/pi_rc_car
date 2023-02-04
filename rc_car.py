96cdd5aa94d3f23f65580dafdd1023377cd27c14
coreykoz <ckozlovski2017@gmail.com>
2023-02-03 22:41:53 -0600

    basic gas

--- rc_car.py
+++ rc_car.py
@@ -1,5 +1,6 @@
 #import evdev
 from evdev import InputDevice, categorize, ecodes
+from adafruit_motorkit import MotorKit
 
 #creates object 'gamepad' to store the data
 #you can call it whatever you like
@@ -17,18 +18,26 @@ lBumper = 310
 startBtn = 315
 shareBtn = 314
 
+gasBtnStatus = False
+
+#Motorkit stuff
+kit = MotorKit()
+driverMotor = kit.motor1
+turnMotor = kit.motor2
+
+
 #prints out device info at start
 print(gamepad)
 
 #evdev takes care of polling the controller in a loop
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_KEY:
-        print(event)
 
         # on button press
         if event.value == 1:
             if event.code == aBtn:
                 print("A Pressed")
+                gasBtnStatus = True
             elif event.code == bBtn:
                 print("B Pressed")
             elif event.code == yBtn:
@@ -40,9 +49,16 @@ for event in gamepad.read_loop():
         elif event.value == 0:
             if event.code == aBtn:
                 print("A Released")
+                gasBtnStatus = False
             elif event.code == bBtn:
                 print("B Released")
             elif event.code == yBtn:
                 print("Y Released")
             elif event.code == xBtn:
                 print("X Released")
+
+        # CAR DRIVING
+        if gasBtnStatus:
+            driverMotor.throttle = .1
+        else:
+            driverMotor.throttle = 0
