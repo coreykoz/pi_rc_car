5d3f54fb464954719c5720eb14ae3cf0b65c71c2
coreykoz <ckozlovski2017@gmail.com>
2023-02-03 22:29:41 -0600

    key press printing

--- rc_car.py
+++ rc_car.py
@@ -3,11 +3,38 @@ from evdev import InputDevice, categorize, ecodes
 
 #creates object 'gamepad' to store the data
 #you can call it whatever you like
-gamepad = InputDevice('/dev/input/event3')
+gamepad = InputDevice('/dev/input/event1')
+
+
+#button code variables
+aBtn = 304
+bBtn = 305
+yBtn = 308
+xBtn = 307
+rBumper = 311
+lBumper = 310
+
+startBtn = 315
+shareBtn = 314
 
 #prints out device info at start
 print(gamepad)
 
 #evdev takes care of polling the controller in a loop
 for event in gamepad.read_loop():
-    print(categorize(event))
+    if event.type == ecodes.EV_KEY:
+        if event.value == 1:
+            if event.code == aBtn:
+                print("A")
+            elif event.code == bBtn:
+                print("B")
+            elif event.code == playpause:
+                print("Play/Pause")
+            elif event.code == up:
+                print("up")
+            elif event.code == down:
+                print("down")
+            elif event.code == left:
+                print("left")
+            elif event.code == right:
+                print("right")
