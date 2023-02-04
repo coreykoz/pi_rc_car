681f6a3961fb5819d796f100b6157e8aa088d764
coreykoz <ckozlovski2017@gmail.com>
2023-02-03 23:46:05 -0600

    testing values

--- rc_car.py
+++ rc_car.py
@@ -102,7 +102,7 @@ for event in gamepad.read_loop():
 
      #read stick axis movement
     elif event.type == ecodes.EV_ABS:
-        print(str(event.code) + str(event.value))
+        print(str(event))
         # if axis[ event.code ] in [ 'ls_x', 'ls_y', 'rs_x', 'rs_y']:
         #     last[ axis[ event.code ] ] = event.value
 
