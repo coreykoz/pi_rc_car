2d0487d6d12b0f30f5517fb4a3727fb8f5b4e6fd
coreykoz <ckozlovski2017@gmail.com>
2023-02-05 10:41:05 -0600

    turning

--- rc_car.py
+++ rc_car.py
@@ -30,34 +30,6 @@ turnScale = .5
 CENTER_TOLERANCE = 350
 STICK_MAX = 65536
 
-dev = InputDevice( list_devices()[0] )
-axis = {
-    ecodes.ABS_X: 'ls_x', # 0 - 65,536   the middle is 32768
-    ecodes.ABS_Y: 'ls_y',
-    ecodes.ABS_Z: 'rs_x',
-    ecodes.ABS_RZ: 'rs_y',
-    ecodes.ABS_BRAKE: 'lt', # 0 - 1023
-    ecodes.ABS_GAS: 'rt',
-
-    ecodes.ABS_HAT0X: 'dpad_x', # -1 - 1
-    ecodes.ABS_HAT0Y: 'dpad_y'
-}
-
-center = {
-    'ls_x': STICK_MAX/2,
-    'ls_y': STICK_MAX/2,
-    'rs_x': STICK_MAX/2,
-    'rs_y': STICK_MAX/2
-}
-
-last = {
-    'ls_x': STICK_MAX/2,
-    'ls_y': STICK_MAX/2,
-    'rs_x': STICK_MAX/2,
-    'rs_y': STICK_MAX/2
-}
-
-
 #prints out device info at start
 print(gamepad)
 
@@ -98,15 +70,12 @@ for event in gamepad.read_loop():
 
         # CAR DRIVING
         if gasBtnStatus:
-            driverMotor.throttle = gasScale * -.25
+            driverMotor.throttle = gasScale * -1
         else:
             driverMotor.throttle = 0
 
      #read stick axis movement
     elif event.type == ecodes.EV_ABS:
-        # last[ axis[ event.code ] ] = event.value
-        # value = event.value - center[ axis[ event.code ] ]
-
         value = event.value
         if abs( value ) <= CENTER_TOLERANCE:
             value = 0
@@ -114,23 +83,6 @@ for event in gamepad.read_loop():
         if event.code == ecodes.ABS_X:
             turnResult = turnScale * (value / (STICK_MAX / 2))
             print(turnResult)
-        # if axis[ event.code ] in [ 'ls_x', 'ls_y', 'rs_x', 'rs_y']:
-        #     last[ axis[ event.code ] ] = event.value
+            turnMotor.throttle = turnResult
 
-        #     value = event.value - center[ axis[ event.code ] ]
-
-        #     if abs( value ) <= CENTER_TOLERANCE:
-        #         value = 0
-
-        #     # if axis[ event.code ] == 'rs_x':
-        #     #     if value < 0:
-        #     #         print('left')
-        #     #     else:
-        #     #         print('right')
-        #     #     print( value )
-
-        #     elif axis[ event.code ] == 'ls_y':
-        #         if value < 0:
-        #             print('left: ' + str(value))
-        #         elif value > 0:
-        #             print('right: ' + str(value))
+        
