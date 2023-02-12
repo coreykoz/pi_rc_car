408e41f274b99b1389b0ec0c6e723fe99bbfdf96
coreykoz <ckozlovski2017@gmail.com>
2023-02-12 17:46:54 -0600

    edits

--- camera_serverv2.py
+++ camera_serverv2.py
@@ -21,7 +21,7 @@ PAGE = """\
 </head>
 <body>
 <h1>Picamera2 MJPEG Streaming Demo</h1>
-<img src="stream.mjpg" width="640" height="480" />
+<img src="stream.mjpg" width="1280" height="720" />
 </body>
 </html>
 """
