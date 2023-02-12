118e67cb7d444511eed9d050a73f69cc8bf4335c
coreykoz <ckozlovski2017@gmail.com>
2023-02-12 00:51:44 -0600

    more testing

--- fan_pwm_testing.py
+++ fan_pwm_testing.py
@@ -5,7 +5,5 @@ from rpi_hardware_pwm import HardwarePWM
 pwm = HardwarePWM(pwm_channel=0, hz=60)
 pwm.start(100) # full duty cycle
 
-pwm.change_duty_cycle(50)
+pwm.change_duty_cycle(10)
 pwm.change_frequency(25_000)
-
-pwm.stop()
