ab9174dcdd8f6befd7aeb473ab04eaa0b5a18dae
coreykoz <ckozlovski2017@gmail.com>
2023-02-12 22:36:03 -0600

    reduce cam resolution

--- camera_serverv2.py
+++ camera_serverv2.py
@@ -85,7 +85,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)}))
+picam2.configure(picam2.create_video_configuration(main={"size": (1280, 720)}))
 output = StreamingOutput()
 picam2.start_recording(JpegEncoder(), FileOutput(output))
 
